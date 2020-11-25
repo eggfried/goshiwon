458005215eb88bcbe62c04fa5d58f10fc6480e95
eggfried@gmail.com <eggfried>
2020-11-25 10:31:57 +0900

    aa

--- Goshiwon.dpr
+++ Goshiwon.dpr
@@ -2,13 +2,21 @@ program Goshiwon;
 
 uses
   Vcl.Forms,
-  MainForm in 'MainForm.pas' {frmMain};
+  MainForm in 'MainForm.pas' {frmMain},
+  Vcl.Themes,
+  Vcl.Styles,
+  MainDataModule in 'MainDataModule.pas' {dmMain: TDataModule},
+  MainLeftForm in 'MainLeftForm.pas' {frmMainLeft},
+  NewsForm in 'NewsForm.pas' {frmNews},
+  LoginForm in 'LoginForm.pas' {frmLogin};
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TdmMain, dmMain);
   Application.CreateForm(TfrmMain, frmMain);
+  Application.CreateForm(TfrmLogin, frmLogin);
   Application.Run;
 end.
--- MainForm.pas
+++ MainForm.pas
@@ -4,10 +4,33 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.RibbonLunaStyleActnCtrls,
+  Vcl.Ribbon, Vcl.Menus, Vcl.ToolWin, Vcl.ActnMan, Vcl.ActnCtrls, Vcl.ExtCtrls,
+  System.ImageList, Vcl.ImgList, Vcl.ComCtrls;
 
 type
   TfrmMain = class(TForm)
+    MainMenu1: TMainMenu;
+    N1: TMenuItem;
+    N2: TMenuItem;
+    N3: TMenuItem;
+    N4: TMenuItem;
+    N5: TMenuItem;
+    N6: TMenuItem;
+    N7: TMenuItem;
+    pnlLeft: TPanel;
+    pnlCenter: TPanel;
+    tlbBigMenu: TToolBar;
+    ToolButton1: TToolButton;
+    ToolButton2: TToolButton;
+    ToolButton3: TToolButton;
+    ToolButton4: TToolButton;
+    ToolButton5: TToolButton;
+    ImageList1: TImageList;
+    ToolButton6: TToolButton;
+    ToolButton7: TToolButton;
+    N8: TMenuItem;
+    procedure FormCreate(Sender: TObject);
   private
     { Private declarations }
   public
@@ -20,5 +43,24 @@ var
 implementation
 
 {$R *.dfm}
+
+uses MainLeftForm, NewsForm;
+
+procedure TfrmMain.FormCreate(Sender: TObject);
+begin
+  if not Assigned(frmMainLeft) then
+    frmMainLeft := TfrmMainLeft.Create(Self);
+  frmMainLeft.Parent := pnlLeft;
+  frmMainLeft.BorderStyle := bsNone;
+  frmMainLeft.Align := alClient;
+  frmMainLeft.Show;
+
+  if not Assigned(frmNews) then
+    frmNews := TfrmNews.Create(Self);
+  frmNews.Parent := pnlCenter;
+  frmNews.BorderStyle := bsNone;
+  frmNews.Align := alClient;
+  frmNews.Show;
+end;
 
 end.
